ef6faa6452eed56c9b3ee23132dd48badd14e8ac
Dilan Ariza <dilansantiagoariza@gmail.com>
2020-12-31 13:15:44 -0500

    DELETE: consoleError.js
    Se elimino el archivo de consoleError.js y se reemplazo por el archivo consoleFunctions.js
    
    Issue: #1

--- utils/messages/console/consoleError.js
+++
@@ -1,7 +0,0 @@
-function consoleError(from, error, time) {
-    console.group("Console - error");
-    console.log(`ERROR | ${from} | ${error} | ${time} | END`);
-    console.groupEnd();
-}
-
-module.exports = { consoleError };
